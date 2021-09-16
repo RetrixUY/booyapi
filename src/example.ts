722e8f7b6cd64cebd0304a504e7657f3f29e5bb5
RetrixUY <retrixuy@gmail.com>
2021-09-16 04:06:29 -0300

    Update example.ts

--- src/example.ts
+++ src/example.ts
@@ -1,12 +1,12 @@
 import {ChatClient} from './ChatClient';
-const botId = 123456789;
-const channelId = 987654321;
-const sessionKey = "INSERT_SESSION_KEY";
+const botId = 123456789; //ID OF THE BOT ACCOUNT (extract from bot profile url, example: https://booyah.live/studio/123456789)
+const channelId = 987654321; //ID OF THE TARGET CHANNEL (extract from channel url, example: https://booyah.live/channels/123456789)
+const sessionKey = "INSERT_SESSION_KEY"; //BOOYAH SESSION KEY (login to the bot account and extract from the browser cookies)
 
 const chatClient = new ChatClient(sessionKey,botId);
 chatClient.on('message',async (chatMessage)=>{
-    if(chatMessage.text == "!ping"){
-        chatClient.sendMessage("Pong!")
+if(chatMessage.text == "!ping"){
+    chatClient.sendMessage("Pong!")
     }
 });
 
